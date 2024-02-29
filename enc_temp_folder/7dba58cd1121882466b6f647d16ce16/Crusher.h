78b4fe91b3fdb701995146a8af311a5ae1503cda
Petr Kotáb <petrkotab99@gmail.com>
2024-02-29 19:00:54 +0100

    feat(map): added victory sphere

---
+++ enc_temp_folder/7dba58cd1121882466b6f647d16ce16/Crusher.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Crusher.generated.h"
+
+UCLASS(Abstract)
+class MFFUNREALFINAL_API ACrusherBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACrusherBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	static constexpr float MIN_HEIGHT = 50.f;
+	static constexpr float MAX_HEIGHT = 200.f;
+	static constexpr float SPEED = 10.f;
+};
